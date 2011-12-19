f5780e4248172d03de28eece765a612a2431f7b7
Alex <alchapone@yandex.ru>
2011-12-20 01:06:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muzkom::Application.config.secret_token = '2f20f1de329af3a618c374d0cba999ea88a31d0fe6e0b53591d351d0a48cc0e1942730f1c9b8d76bf0bc01675607a29ff0adea58edce725fbdc80e69d1dc0102'
