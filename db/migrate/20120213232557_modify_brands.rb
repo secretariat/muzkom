b7b729f27cc6c67179b0b4cd5a1709ffd87a5c8f
Alex <alchapone@yandex.ru>
2012-02-14 01:28:19 +0200

    Added brands migration

---
+++ db/migrate/20120213232557_modify_brands.rb
@@ -0,0 +1,12 @@
+class ModifyBrands < ActiveRecord::Migration
+  def up
+    add_column :brands, :title, :string, :default => "", :null => false
+    add_column :brands, :meta_d, :string, :default => "", :null => false
+    add_column :brands, :meta_k, :string, :default => "", :null => false
+    add_column :brands, :logo, :string, :default => "", :null => false
+    add_column :brands, :text, :text, :default => "", :null => false
+  end
+
+  def down
+  end
+end
