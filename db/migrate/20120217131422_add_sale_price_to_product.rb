d05dfd3ee50e02e6ae83129023d723f3a18d185e
Alex <alchapone@yandex.ru>
2012-02-18 20:08:20 +0200

    Fixed migration

--- db/migrate/20120217131422_add_sale_price_to_product.rb
+++ db/migrate/20120217131422_add_sale_price_to_product.rb
@@ -1,5 +1,5 @@
 class AddSalePriceToProduct < ActiveRecord::Migration
   def change
-    add_column :products, :sale_price, :decimal, :precision => 10, :scale => 2
+    add_column :products, :sale_price, :decimal, :precision => 10, :scale => 2, :default=>0
   end
 end
