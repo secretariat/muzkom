387e2f3d1af53d71f506572560181bf4e790548d
Alex <alchapone@yandex.ru>
2012-09-09 15:53:14 +0300

    Fixed logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Muzkom::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
-  config.logger = Logger.new(config.log_path, "weekly")
+  config.logger = Logger.new(config.paths.log.first, 10, 100.megabytes)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
