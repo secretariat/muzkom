e2725b4abe4b9d15082e683d8013a09c550fad2c
Alex <alchapone@yandex.ru>
2013-03-20 21:48:36 +0200

    Fixes

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -9,16 +9,6 @@ module AdminHelper
     property.id unless property.nil?
   end
 
-  def four_digit_id(id)
-    if id.to_s.length < 4
-      until id.to_s.length == 4
-        id = "0#{id}"
-      end
-      return id
-    end
-    return id
-  end
-
   def human_status(status)
     return "Ожидает" if status == 'pending'
     return "Выполняется" if status == 'started'
