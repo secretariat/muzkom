61f046b1947cc5b925253206f9dc69f529c8f385
Alex <alchapone@yandex.ru>
2013-03-20 18:02:19 +0200

    Fixed ids

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,14 +1,14 @@
 #encoding: utf-8
 module AdminHelper
-  
+
   def visibility_button(value)
     value == true ? image_tag('admin/visible.png') : image_tag('admin/invisible.png')
   end
-  
+
   def id_or_blank(property)
     property.id unless property.nil?
   end
-  
+
   def four_digit_id(id)
     if id.to_s.length < 4
       until id.to_s.length == 4
@@ -16,8 +16,9 @@ module AdminHelper
       end
       return id
     end
+    return id
   end
-  
+
   def human_status(status)
     return "Ожидает" if status == 'pending'
     return "Выполняется" if status == 'started'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Muzkom::Application.routes.draw do
-  
+
   root :to=>'pages#index'
   match '/login' => 'sessions#new', :as=>:login
   match '/logout' => 'sessions#destroy', :as=>:logout
@@ -10,36 +10,36 @@ Muzkom::Application.routes.draw do
   get "/contacts" => "pages#show", :uri=>'contacts'
   get "/feed" => "pages#feed"
   put "/currency_change" => 'shop#change', :as=>:change_currency
-  
+
   resources :sessions, :only => [:new, :create, :destroy]
   resources :feedbacks, :only => [:new, :create]
-  
+
   resources :categories, :only=>[:show] do
     resources :brands, :only=>[:show]
   end
-  
+
   resources :products, :only=>[:show] do
     resources :product_comments, :only=>[:create]
     get :withdrawn, :on => :collection
   end
-  
+
   resources :brands, :only=>[:show] do
     get :withdrawn, :on => :collection
   end
-  
+
   resources :publications, :only=>[:index, :show] do
     resources :publication_comments, :only=>[:create]
   end
   resources :promotions, :only=>[:index, :show]
   resources :checkouts, :only=>[:new, :create]
-  
+
   resource :cart, :controller => "cart" do
     get :index
     post :change
     put :add, :on => :member
     delete :delete, :on => :member
   end
-  
+
   namespace :admin do
     root :to=>'index#index'
     resources :checkouts
@@ -52,19 +52,19 @@ Muzkom::Application.routes.draw do
     resources :feedbacks, :only => [:index, :destroy]
     resources :product_comments, :only => [:index, :destroy]
     resources :publication_comments, :only => [:index, :destroy]
-    
+
     resources :banners do
       post :placement, :on => :member
     end
-    
+
     resources :promotions do
       post :visibility, :on => :member
     end
-    
+
     resources :publications do
       post :visibility, :on => :member
     end
-    
+
     resources :brands do
       resources :currencies, :only=>[:index] do
         post :enter, :on => :collection
@@ -76,8 +76,8 @@ Muzkom::Application.routes.draw do
       end
       post :visibility, :on => :member
     end
-    
-    resources :subcategories do 
+
+    resources :subcategories do
       post :sort, :on => :collection
       post :move, :on => :collection
       resources :products do
@@ -85,7 +85,7 @@ Muzkom::Application.routes.draw do
         post :show_index, :on => :member
       end
     end
-    
+
     resources :categories do
       post :visibility, :on => :member
       post :sort, :on => :collection
@@ -93,20 +93,20 @@ Muzkom::Application.routes.draw do
           post :visibility, :on => :member
       end
     end
-    
+
     resources :products do
       post :visibility, :on => :member
       post :show_index, :on => :member
       get :switch, :on => :collection
     end
-    
-    
+
+
     resources :currencies, :only => [:index] do
       post :change, :on => :collection
     end
 
   end
-  
+
   match "/page.php" => "pages#show", :uri=>'about_us'
   match "/contact.php" => "feedbacks#new"
   match "/cart.php" => "cart#index"
