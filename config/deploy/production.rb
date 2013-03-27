5714c9009978f8b6c5acc77b499642df580487b9
Alex <alchapone@yandex.ru>
2013-03-27 12:13:20 +0200

    Fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ module UseScpForDeployment
     base.send(:alias_method, :old_upload, :upload)
     base.send(:alias_method, :upload, :new_upload)
   end
-  
+
   def new_upload(from, to)
     old_upload(from, to, :via => :scp)
   end
@@ -23,22 +23,22 @@ namespace :deploy do
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
+
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   end
-  
+
   task :bundle do
     run "cd #{release_path} && bundle install --gemfile #{release_path}/Gemfile --path /home/muzkomco"
   end
-  
+
   task :assets do
-    run "cd #{release_path} && rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile"
+    run "cd #{release_path} && bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile"
   end
-  
+
   task :migrations do
     run "cd #{release_path} && rake RAILS_ENV=production db:migrate"
   end
-  
+
 end
