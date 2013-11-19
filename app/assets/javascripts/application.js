5c3de40048ebec24c76ca882127fa253dd1a838a
«Secretariat1987» <mixey.viti@bigmir.net>
2013-11-19 20:37:12 +0200

    comment length js moved to layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -139,16 +139,17 @@ $(document).ready(function(){
 
 });
 
-CharacterCount = function(TextArea,FieldToCount){
-  var myField = document.getElementById(TextArea);
-  var myLabel = document.getElementById(FieldToCount);
-  if(!myField || !myLabel){return false}; // catches errors
-  var MaxChars =  myField.maxLengh;
-  if(!MaxChars){MaxChars =  myField.getAttribute('maxlength') ; };  if(!MaxChars){return false};
-  var remainingChars =   MaxChars - myField.value.length
-  myLabel.innerHTML = "Комментарий (" + remainingChars+" символов осталось из "+MaxChars+")"
-}
-setInterval(function(){CharacterCount('myfield','CharCountLabel1')},55);
+// CharacterCount = function(TextArea,FieldToCount){
+//   var myField = document.getElementById(TextArea);
+//   var myLabel = document.getElementById(FieldToCount);
+//   if(!myField || !myLabel){return false}; // catches errors
+//   var MaxChars =  myField.maxLengh;
+//   if(!MaxChars){MaxChars =  myField.getAttribute('maxlength') ; };  if(!MaxChars){return false};
+//   var remainingChars =   MaxChars - myField.value.length
+//   myLabel.innerHTML = "Комментарий (" + remainingChars+" символов осталось из "+MaxChars+")"
+// }
+
+// setInterval(function(){CharacterCount('myfield','CharCountLabel1')},55);
 
 
 function setCookie(name,value, days) {
