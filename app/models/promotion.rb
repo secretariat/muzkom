910b17575525fc68514faba7c81e950d59d1c39b
«Secretariat1987» <mixey.viti@bigmir.net>
2013-12-24 21:42:01 +0200

    promo start

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,17 +1,17 @@
 # -*- encoding : utf-8 -*-
 class Promotion < ActiveRecord::Base
-  
+
   paginates_per 20
-  
+
   mount_uploader :image, PromotionImageUploader
   has_and_belongs_to_many :products
-  
+
   scope :visible, where(:visibility=>true)
-  
+
   def has_image?
     unless image_url.blank?
       return image_url == "/assets/fallback/promotion/default.jpg" ? false : true
     end
   end
-    
+
 end
